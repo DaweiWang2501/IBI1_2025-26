94ba57c8b8bff1a198ec93602faeb7363422fcbb
DaweiWang2501 <dawei1.25@intl.zju.edu.cn>
2026-03-11 14:13:15 +0800

    Update creatine_clearance.py

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -9,20 +9,23 @@
 #   Formula: CrCl = ((140 - age) * weight) / (72 * (creatine / 88.4))
 #   If the patient is female, multiply the result by 0.85
 #   Print the final calculated CrCl value
-age = 20
-weight = 63
-creatine = 25
-#This is not my creatine concentration
-#It's just in the normal scale
-gender = "male"
+import sys
+age = float(input("Enter age (years):"))
 if age >= 100:
     print("Error: age must be less than 100")
-elif weight <=20 or weight >= 80:
+    sys.exit()
+weight = float(input("Enter weight (kg):"))
+if weight <=20 or weight >= 80:
     print("Error: weight must be between 20 and 80")
-elif creatine <= 0 or creatine >= 100:
+    sys.exit()
+creatine = float(input("Enter creatinine concentration (umol/L):"))
+if creatine <= 0 or creatine >= 100:
     print("Error: creatine concentration must be between 0 and 100")
-elif gender != "male" and gender != "female":
-    print("Error: gender must be male or female") 
+    sys.exit()
+gender = input("Enter gender (male/female):").lower()
+if gender != "male" and gender != "female":
+    print("Error: gender must be male or female")
+    sys.exit() 
 else:
     CrCl = ((140 - age) * weight) / (72 * (creatine / 88.4))
     if gender == "female":
